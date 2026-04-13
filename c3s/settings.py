cd8cfc236a835ad212a4172cf3da78d805388ed1
twonum <git@twonum.org>
2026-04-13 09:52:25 -0700

    style: Formatting

--- c3s/settings.py
+++ c3s/settings.py
@@ -4,20 +4,20 @@
 #  This file is part of Celeste's Custom Can Storage (C3s).
 #
 #  C3s is free software: you can redistribute it and/or modify it under
-#  the terms of the GNU General Public License as published by the Free
-#  Software Foundation, either version 3 of the License, or (at your
+#  the terms of the GNU Affero General Public License as published by the
+#  Free Software Foundation, either version 3 of the License, or (at your
 #  option) any later version.
 #
 #  C3s is distributed in the hope that it will be useful, but WITHOUT
 #  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-#  FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
-#  for more details.
+#  FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
+#  License for more details.
 #
-#  You should have received a copy of the GNU General Public License
-#  along with C3s. If not, see <https://www.gnu.org/licenses/>.
+#  You should have received a copy of the GNU Affero General Public
+#  License along with C3s. If not, see <https://www.gnu.org/licenses/>.
 
 # Some of this was copied from twonum.org's settings.py
-# copyright 2025-2026 twonum.org / Celest
+# copyright 2025-2026 twonum.org / Celeste
 # Affero General Public License version 3
 # <https://github.com/twonfi/twonum.org>
 
@@ -62,7 +62,7 @@ def _env_file(key: str) -> str:
 SILENCED_SYSTEM_CHECKS = [
     # HTTPS-related stuff (handled by proxy/balancer)
     "security.W004",
-    "security.W008"
+    "security.W008",
 ]
 
 DEBUG = env("DEBUG")
--- canstorage/admin.py
+++ canstorage/admin.py
@@ -3,17 +3,17 @@
 #  This file is part of Celeste's Custom Can Storage (C3s).
 #
 #  C3s is free software: you can redistribute it and/or modify it under
-#  the terms of the GNU General Public License as published by the Free
-#  Software Foundation, either version 3 of the License, or (at your
+#  the terms of the GNU Affero General Public License as published by the
+#  Free Software Foundation, either version 3 of the License, or (at your
 #  option) any later version.
 #
 #  C3s is distributed in the hope that it will be useful, but WITHOUT
 #  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-#  FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
-#  for more details.
+#  FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
+#  License for more details.
 #
-#  You should have received a copy of the GNU General Public License
-#  along with C3s. If not, see <https://www.gnu.org/licenses/>.
+#  You should have received a copy of the GNU Affero General Public
+#  License along with C3s. If not, see <https://www.gnu.org/licenses/>.
 
 from django.contrib import admin
 
@@ -80,10 +80,14 @@ class ObjectAdmin(admin.ModelAdmin):
     def has_add_permission(self, request) -> bool:
         return request.user.has_perm("canstorage.add_object")
 
-    def has_change_permission(self, request, obj: models.Object = None) -> bool:
+    def has_change_permission(
+        self, request, obj: models.Object = None
+    ) -> bool:
         if request.user.has_perm("canstorage.change_object"):
             return True
-        elif obj is not None and obj.can.access_control_list.check_permission(models.AccessControlList.WRITE, request.user):
+        elif obj is not None and obj.can.access_control_list.check_permission(
+            models.AccessControlList.WRITE, request.user
+        ):
             return True
         else:
             return False
@@ -91,15 +95,21 @@ class ObjectAdmin(admin.ModelAdmin):
     def has_view_permission(self, request, obj: models.Object = None) -> bool:
         if request.user.has_perm("canstorage.view_object"):
             return True
-        elif obj is not None and obj.can.access_control_list.check_permission(models.AccessControlList.READ, request.user):
+        elif obj is not None and obj.can.access_control_list.check_permission(
+            models.AccessControlList.READ, request.user
+        ):
             return True
         else:
             return False
 
-    def has_delete_permission(self, request, obj: models.Object = None) -> bool:
+    def has_delete_permission(
+        self, request, obj: models.Object = None
+    ) -> bool:
         if request.user.has_perm("canstorage.delete_object"):
             return True
-        elif obj is not None and obj.can.access_control_list.check_permission(models.AccessControlList.WRITE, request.user):
+        elif obj is not None and obj.can.access_control_list.check_permission(
+            models.AccessControlList.WRITE, request.user
+        ):
             return True
         else:
             return False
